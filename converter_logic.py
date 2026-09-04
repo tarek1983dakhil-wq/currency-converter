8ca7c6fe082abb3dd2e8b7b4dd5c6a2494cf9955
Tarek Aldakhil <tarek1983dakhil@gmail.com>
2026-09-04 14:11:36 +0200

    Update currency API to support EGP and SYP

--- converter_logic.py
+++ converter_logic.py
@@ -1,16 +1,30 @@
 import requests
 
+
 def get_available_currencies():
-    url = "https://api.frankfurter.app/currencies"
+    url = "https://api.frankfurter.dev/v2/currencies"
+
     response = requests.get(url)
+    response.raise_for_status()
+
     data = response.json()
-    return list(data.keys())
+
+    currencies = [currency["iso_code"] for currency in data]
+
+    return sorted(currencies)
+
 
 def convert_currency(amount, from_currency, to_currency):
     if from_currency == to_currency:
         return amount
-    url = f"https://api.frankfurter.app/latest?amount={amount}&from={from_currency}&to={to_currency}"
+
+    url = f"https://api.frankfurter.dev/v2/rate/{from_currency}/{to_currency}"
+
     response = requests.get(url)
+    response.raise_for_status()
+
     data = response.json()
-    return data["rates"][to_currency]
+    rate = data["rate"]
+
+    return amount * rate
 
